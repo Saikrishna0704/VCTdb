65a18f84d3c1d8e811ea9bae887687319672820b
Saikrishna0704 <83568778+Saikrishna0704@users.noreply.github.com>
2024-05-10 13:05:32 -0400

    Add files via upload

---
+++ app.py
@@ -0,0 +1,54 @@
+import streamlit as st
+import psycopg2
+def connect_to_db():
+    conn = psycopg2.connect(
+        dbname="one",
+        user="ukaveti2",
+        password="110899",
+        host="127.0.0.1"  # or your host address
+    )
+    return conn
+
+# Function to fetch data from PostgreSQL database
+def fetch_data():
+    conn = connect_to_db()
+    cur = conn.cursor()
+    cur.execute("select * from sai;")
+    data = cur.fetchall()
+    cur.close()
+    conn.close()
+    return data
+
+# Function to insert data into PostgreSQL database
+def insert_data(name, age):
+    pass
+'''
+    conn = connect_to_db()
+    cur = conn.cursor()
+    cur.execute("INSERT INTO your_table_name (name, age) VALUES (%s, %s)", (name, age))
+    conn.commit()
+    cur.close()
+    conn.close()
+    '''
+
+# Streamlit web application
+def main():
+    st.title("PostgreSQL Web Application")
+
+    # Insert data
+    st.header("Insert Data")
+    name = st.text_input("Enter Name:")
+    age = st.number_input("Enter Age:")
+    if st.button("Insert"):
+        insert_data(name, age)
+        st.success("Data Inserted Successfully!")
+
+    # Display data
+    st.header("Display Data")
+    data = fetch_data()
+    print(data)
+    st.write("Data from PostgreSQL Database:")
+    st.write(data)
+
+if __name__ == "__main__":
+    main()
